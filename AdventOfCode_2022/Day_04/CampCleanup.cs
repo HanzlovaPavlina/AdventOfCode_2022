4b1e33fc8676db84c06811a0a33c582e6931ba2a
Pavlína Hanzlová <pav.hanzlova@gmail.com>
2022-12-04 23:22:32 +0100

    Day_04 Cam Cleanup done

---
+++ AdventOfCode_2022/Day_04/CampCleanup.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+struct Range {
+    public int from;
+    public int to;
+
+    public Range(int from, int to) {
+        this.from = from; this.to = to;
+        }
+
+    public int Length() {
+        return to - from;
+        }
+    }
+
+namespace AdventOfCode_2022 {
+    class CampCleanup {
+
+        public int GetCleanUpFullyContains(string input) {
+            LoadAndProcessInput(input);
+            return fullyContains;
+            }
+
+        private int fullyContains = 0;
+
+        private void LoadAndProcessInput(string path) {
+
+            int[] matchesValues = new int[4];
+            int i = 0;
+
+            string pattern = @"\d*";
+
+            foreach (string line in File.ReadLines(path)) {
+                try {
+                    Match match = Regex.Match(line, pattern, RegexOptions.None, TimeSpan.FromSeconds(1));
+                    i = 0;
+
+                    // Handle match here...
+                    while (match.Success) {
+                        if (match.Value != "") {
+                            matchesValues[i] = Convert.ToInt32(match.Value);
+                            i++;
+                            }
+                        match = match.NextMatch();
+                        }
+                    fullyContains += CheckPartOvelap(new Range(matchesValues[0], matchesValues[1]), new Range(matchesValues[2], matchesValues[3]));
+                    }
+                catch (RegexMatchTimeoutException) {
+                    // Do nothing: assume that exception represents no match.
+                    }
+                }
+            }
+
+        // --- Part One ---
+        private int CheckFullyOverlap(Range first, Range second) {
+            if (first.from >= second.from && first.to <= second.to) return 1; 
+            if(first.from <= second.from && first.to >= second.to) return 1;
+            return 0;
+            }
+
+        // --- Part Two ---
+        private int CheckPartOvelap(Range first, Range second) {
+            if (CheckFullyOverlap(first, second) == 1) return 1;
+            if (first.from <= second.from && first.to >= second.from) return 1;
+            if (first.to >= second.to && first.from <= second.to) return 1;
+            return 0;
+            }
+    }
+}
--- AdventOfCode_2022/Program.cs
+++ AdventOfCode_2022/Program.cs
@@ -11,14 +11,16 @@ namespace AdventOfCode_2022 {
             //CalorieCounting_01 counting = new CalorieCounting_01();
             //Console.WriteLine(counting.GetThreeMaxCalories());
 
-            RockPaperScissors_01 newGame = new RockPaperScissors_01();
-            Console.WriteLine(newGame.GetFinalScore("../../Day_02/input.txt"));
+            //RockPaperScissors_01 newGame = new RockPaperScissors_01();
+            //Console.WriteLine(newGame.GetFinalScore("../../Day_02/input.txt"));
 
             //RucksackReorganization_02 rucksacks = new RucksackReorganization_02();
             //Console.WriteLine(rucksacks.GetSum("../../Day_03/input.txt"));
 
+            CampCleanup camp = new CampCleanup();
+            Console.WriteLine(camp.GetCleanUpFullyContains("../../Day_04/input.txt"));
+
             Console.ReadKey();
-            
             }
         }
     }
